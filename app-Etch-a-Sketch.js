588f2c655c98dd2f7d672ba24c1a4cc119b49318
alber <aicarq@hotmail.com>
2022-11-02 15:12:57 -0600

    locally emerged files and added new branch for several changes

--- app-Etch-a-Sketch.js
+++ app-Etch-a-Sketch.js
@@ -2,38 +2,56 @@ const container = document.getElementById('container');
 let redRgb = Math.floor(Math.random()*255); // ONLY ONE VALUE PER PAGE LOAD
 let greenRgb = Math.floor(Math.random()*255); // ONLY ONE VALUE PER PAGE LOAD
 let blueRgb = Math.floor(Math.random()*255); // ONLY ONE VALUE PER PAGE LOAD
-let randomColors = `rgb(${redRgb}, ${greenRgb}, ${blueRgb})`; // ONLY ONE VALUE PER PAGE LOAD
+let oneRandomColor = `rgb(${redRgb}, ${greenRgb}, ${blueRgb})`; // ONLY ONE VALUE PER PAGE LOAD
 
 /* FUNCTION TO CALL DIFERENT COLORS EVERY TIME IS CALLED */
 
 function randomColorsGenerator() {
     return `rgb(${Math.floor(Math.random()*255)},
-     ${Math.floor(Math.random()*255)},
-     ${Math.floor(Math.random()*255)})`;
+    ${Math.floor(Math.random()*255)},
+    ${Math.floor(Math.random()*255)})`;
 }
 
-/* FUNCTION TO MAKE CELLS DEPENDING OF NUMBERS REQUIRED */
+// FUNCTION TO CALL ONE SINGLE COLOR
 
-function makeDivs(numDivs) {
-    for (let d = 0; d < numDivs; d++){
-        let cells = document.createElement('div');
-        container.appendChild(cells).className = 'grid-item';  
-    }
+function oneSingleColor() {
+    return oneRandomColor;
 }
 
-makeDivs(256);
+// DARKER MODE
+
+
+
+// FUNCTION TO ASK SQUARES PER SIDE OF GRID
+
+function makeGridXAndY(rows, cols) {
+        container.style.setProperty('--grid-rows', rows);
+        container.style.setProperty('--grid-cols', cols);
+        for (let c = 0; c < (rows*cols); c++) {
+                let cells = document.createElement('div');
+                container.appendChild(cells).className = 'grid-item';
+            }    
+        }
+        
+makeGridXAndY(10, 10);
+
+// 
 
 const divSelector = document.querySelectorAll('#container>div');
 
-// for(div of divSelector) {
-//     div.style.backgroundColor = `rgb(${redRgb}, ${greenRgb}, ${blueRgb})`;
+//RAINBOW COLORS
+
+// for(let i = 0; i < divSelector.length; i++){
+//     divSelector[i].style.backgroundColor = oneRandomColor;
+//     divSelector[i].addEventListener('mouseover', selectItem);
 //     }
 
-for(let i = 0; i < divSelector.length; i++){
-    divSelector[i].style.backgroundColor = randomColors;
-    divSelector[i].addEventListener('mouseover', selectItem);
-}
+//BLACK COLOR
 
+for(let i = 0; i < divSelector.length; i++){
+    divSelector[i].style.backgroundColor = oneRandomColor;
+    divSelector[i].addEventListener('mouseover', coldplayMode);
+    }
 
 
 /* FUNCTION TO SELECT INDIVIDUAL CELL AND CALL RANDOMCOLOR FOR EVENT */
@@ -44,5 +62,73 @@ function selectItem(e) {
 }
 
 
+//FUNCTION TO SELECT CELL INTO BLACK COLOR 
+function blackItem(e) {
+    e.target.style.backgroundColor = 'black';
+    console.log(e.target);
+}
+
+// FUNCTION TO YELLOW
+function coldplayMode(e) {
+    e.target.style.backgroundColor = 'yellow';
+    console.log(e.target);
+}
+
+// FUNCTION TO DARKER SIDE
+function toEternalDarkness(e) {
+    console.log(e.target);
+}
 
 
+// to restart
+
+const restart = document.getElementById('restart');
+
+restart.addEventListener('click', () => {
+    console.log("clicked restart");
+    for(let i = 0; i < divSelector.length; i++){
+        divSelector[i].style.backgroundColor = 'white';
+        }
+    })
+
+const rainbowSix = document.getElementById('rainbow');
+
+rainbowSix.addEventListener('click', () => {
+    console.log("clicked rainbow");
+    for(let i = 0; i < divSelector.length; i++){
+        divSelector[i].removeEventListener('mouseover', blackItem);
+        divSelector[i].removeEventListener('mouseover', coldplayMode);
+        divSelector[i].removeEventListener('mouseover', toEternalDarkness);
+        divSelector[i].addEventListener('mouseover', selectItem);
+        }
+})
+
+const blackout = document.getElementById('blackout');
+
+blackout.addEventListener('click', () => {
+    console.log('clicked blackout');
+    for(let i = 0; i < divSelector.length; i++){
+        divSelector[i].removeEventListener('mouseover', selectItem);
+        divSelector[i].removeEventListener('mouseover', coldplayMode);
+        divSelector[i].removeEventListener('mouseover', toEternalDarkness);
+        divSelector[i].addEventListener('mouseover', blackItem);
+        }
+})
+
+const darker =document.getElementById('darker');
+
+darker.addEventListener('click', () => {
+    console.log('darker');
+    for(let i = 0; i < divSelector.length; i++){
+        divSelector[i].removeEventListener('mouseover', selectItem);
+        divSelector[i].removeEventListener('mouseover', coldplayMode);
+        divSelector[i].removeEventListener('mouseover', blackItem);
+        divSelector[i].addEventListener('mouseover', toEternalDarkness);
+        }
+})
+
+const gridPrompter = document.querySelector('.button-options');
+
+gridPrompter.children[0].addEventListener('click', () => {
+    console.log('clicked prompter');
+})
